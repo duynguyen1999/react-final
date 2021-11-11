d8bbe8643e1ebddfbb5588ef6cc9f198fb0143ac
tng1027 <tng.1027@gmail.com>
2021-11-11 23:45:19 +0700

    add 404

--- react-online-store/src/app/pages/404.jsx
+++ react-online-store/src/app/pages/404.jsx
@@ -1,5 +1,41 @@
+import { useHistory } from "react-router"
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Segment,
+} from "semantic-ui-react"
+
 const NotFound = () => {
-  return <h1>404</h1>
+  const history = useHistory()
+
+  return (
+    <Container className="auth-form">
+      <Grid columns="equal">
+        <Grid.Column></Grid.Column>
+        <Grid.Column width={12}>
+          <Segment placeholder>
+            <Header icon>
+              <Icon name="search" />
+              We don't have any pages matching your request
+            </Header>
+            <Segment.Inline>
+              <Button
+                content="Home"
+                icon="home"
+                labelPosition="left"
+                positive
+                onClick={() => history.push("/")}
+              />
+            </Segment.Inline>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid>
+    </Container>
+  )
 }
 
 export default NotFound
